8609469b1c8ab146d6d102757ab2de8e4f88853b
srad <ssedighi@posteo.de>
2022-02-04 03:21:56 +0100

    stream counts

--- src/views/StreamView.vue
+++ src/views/StreamView.vue
@@ -6,17 +6,17 @@
       <ul class="nav nav-tabs border-primary" id="myTab" role="tablist">
         <li class="nav-item" role="presentation">
           <button class="nav-link active" id="home-tab" data-bs-toggle="tab" data-bs-target="#home" type="button" role="tab" aria-controls="home" aria-selected="true">
-            Recording
+            <span class="d-none d-lg-inline">Recording</span><span class="d-lg-none">Rec</span> ({{ recordingStreams.length }})
           </button>
         </li>
         <li class="nav-item" role="presentation">
           <button class="nav-link" id="profile-tab" data-bs-toggle="tab" data-bs-target="#profile" type="button" role="tab" aria-controls="profile" aria-selected="false">
-            Not recording
+            Inactive ({{ notRecordingStreams.length }})
           </button>
         </li>
         <li class="nav-item" role="presentation">
           <button class="nav-link" id="contact-tab" data-bs-toggle="tab" data-bs-target="#contact" type="button" role="tab" aria-controls="contact" aria-selected="false">
-            Disabled
+            Disabled ({{ disabledStreams.length }})
           </button>
         </li>
       </ul>
