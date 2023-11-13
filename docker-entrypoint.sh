bcfd4811bd07fad06a47d67a82221205039e24fb
srad <ssedighi@posteo.de>
2023-11-13 20:09:21 +0100

    Fix: issue with env variable.

--- docker-entrypoint.sh
+++ docker-entrypoint.sh
@@ -1,11 +1,12 @@
 #!/bin/sh
-cat <<EOF > /usr/share/nginx/html/env.js
-window.VUE_APP_APIURL = "$API_URL";
+
+cat > /usr/share/nginx/html/env.js <<'endmsg'
+window.VUE_APP_APIURL = "$APP_API_URL";
 window.VUE_APP_BASE = "$APP_BASE";
 window.VUE_APP_NAME = "$APP_NAME";
 window.VUE_APP_SOCKETURL = "$APP_SOCKETURL";
 window.VUE_APP_FILEURL = "$APP_FILEURL";
 window.VUE_APP_BUILD = "$APP_BUILD";
-EOF
+endmsg
 
 nginx -g "daemon off;"
