61a5a0ee566221821cf78657c5aede1915690b30
srad <ssedighi@posteo.de>
2024-08-17 23:51:31 +0200

    Admin view layout

--- src/views/AdminView.vue
+++ src/views/AdminView.vue
@@ -15,107 +15,95 @@
       </div>
       -->
 
-      <div class="row">
-        <div class="col">
-          <button :disabled="isUpdating" class="btn btn-primary me-2" @click="updateInfo" :class="{'blink btn-danger': isUpdating, 'btn-secondary': !isUpdating}">
-            Update Video Metadata
-          </button>
-          <button class="btn btn-primary me-2" @click="posters">
-            Regenerate all posters
-          </button>
-          <button :disabled="importing" class="btn btn-primary me-2" @click="startImport">
-            Start Import
-          </button>
-        </div>
-      </div>
+      <div class="table-responsive border-secondary border m-0 p-0">
+        <table class="table table-bordered m-0">
+          <thead>
+          <tr style="visibility: collapse">
+            <th style="width: 25%"></th>
+            <th></th>
+          </tr>
+          <tr rowspan="2">
+            <th class="text-end bg-light" colspan="2">
+              <div class="btn-group btn-group-sm">
+                <button type="button" :disabled="isUpdating" class="btn btn-primary me-2" @click="updateInfo" :class="{'blink btn-danger': isUpdating}">
+                  <span class="me-2">Update video metadata</span>
+                  <i class="bi bi-arrow-clockwise"></i>
+                </button>
+                <button class="btn btn-primary me-2" @click="posters">
+                  <span class="me-2">Regenerate posters</span>
+                  <i class="bi bi-card-image"></i>
+                </button>
 
-      <table class="table table-bordered my-3">
-        <thead style="visibility: collapse">
-        <tr>
-          <th style="width: 25%"></th>
-          <th></th>
-        </tr>
-        </thead>
-        <tbody>
-        <tr v-if="cpuInfo && cpuInfo.loadCpu">
-          <td class="bg-light align-middle">CPU Load ({{ cpuInfo.loadCpu[0].cpu }}, {{ mainCpuLoad.toFixed(1) }}%)</td>
-          <td class="align-middle">
-            <div class="progress">
-              <div class="progress-bar" role="progressbar" :style="{width: mainCpuLoad + '%' }" aria-valuenow="0"
-                   aria-valuemin="0"
-                   aria-valuemax="100"></div>
-            </div>
-          </td>
-        </tr>
-        <tr v-if="diskInfo">
-          <td class="bg-light align-middle">Disk usage ({{ diskInfo.pcent }})</td>
-          <td class="align-middle">
-            <div class="progress">
-              <div class="progress-bar" role="progressbar" :style="{width: diskInfo.pcent}" aria-valuenow="0"
-                   aria-valuemin="0"
-                   aria-valuemax="100"></div>
-            </div>
-          </td>
-        </tr>
-        <tr v-if="importing">
-          <td class="bg-light align-middle">
-            <button disabled class="btn btn-sm btn-secondary me-2" :class="{'blink btn-danger': importing, 'btn-secondary': !importing}" @click="posters">
+                <button :disabled="importing" class="btn btn-primary" @click="startImport">
+                  <span class="me-2">Start Import</span>
+                  <i class="bi bi-disc"></i>
+                </button>
+              </div>
+            </th>
+          </tr>
+          </thead>
+          <tbody>
+          <tr v-if="cpuInfo && cpuInfo.loadCpu">
+            <td class="bg-light-subtle align-middle">CPU Load ({{ cpuInfo.loadCpu[0].cpu }}, {{
+                mainCpuLoad.toFixed(1)
+              }}%)
+            </td>
+            <td class="align-middle">
+              <div class="progress">
+                <div class="progress-bar" role="progressbar" :style="{width: mainCpuLoad + '%' }" aria-valuenow="0"
+                     aria-valuemin="0"
+                     aria-valuemax="100"></div>
+              </div>
+            </td>
+          </tr>
+          <tr v-if="diskInfo">
+            <td class="bg-light-subtle align-middle">Disk usage ({{ diskInfo.pcent }})</td>
+            <td class="align-middle">
+              <div class="progress">
+                <div class="progress-bar" role="progressbar" :style="{width: diskInfo.pcent}" aria-valuenow="0"
+                     aria-valuemin="0"
+                     aria-valuemax="100"></div>
+              </div>
+            </td>
+          </tr>
+          <tr v-if="importing">
+            <td class="align-middle" :class="{'blink bg-danger-subtle': importing}">
               Importing ({{ importProgress }}/{{ importSize }})
-            </button>
-          </td>
-          <td class="align-middle">
-            <div class="progress">
-              <div class="progress-bar" role="progressbar" :style="{width: (importProgress/importSize*100) + '%'}" aria-valuenow="{{importProgress}}"
-                   aria-valuemin="0"
-                   aria-valuemax="{{importSize}}"></div>
-            </div>
+            </td>
+            <td class="align-middle">
+              <div class="progress">
+                <div class="progress-bar" role="progressbar" :style="{width: (importProgress/importSize*100) + '%'}" aria-valuenow="{{importProgress}}"
+                     aria-valuemin="0"
+                     aria-valuemax="{{importSize}}"></div>
+              </div>
 
-          </td>
-        </tr>
-        <tr>
-          <td colspan="2" class="p-0 ">
-            <hr/>
-          </td>
-        </tr>
-        <tr v-if="netInfo">
-          <td class="bg-light align-middle">Network-Transmitted</td>
-          <td>{{ transmittedMb }} MB</td>
-        </tr>
-        <tr v-if="netInfo">
-          <td class="bg-light align-middle">Network-Received</td>
-          <td>{{ receivedMb }} MB</td>
-        </tr>
-        <tr>
-          <td colspan="2" class="p-0 ">
-            <hr/>
-          </td>
-        </tr>
-        <tr>
-          <td class="bg-light align-middle">
-            Client-Version
-          </td>
-          <td class="align-middle">{{ version }}</td>
-        </tr>
-        <tr>
-          <td class="bg-light align-middle">
-            Client-Revision
-          </td>
-          <td class="align-middle">{{ build }}</td>
-        </tr>
-        <tr>
-          <td class="bg-light align-middle">
-            Server-Version
-          </td>
-          <td class="align-middle">{{ serverInfo?.version }}</td>
-        </tr>
-        <tr>
-          <td class="bg-light align-middle">
-            Server-Revision
-          </td>
-          <td class="align-middle">{{ serverInfo?.commit }}</td>
-        </tr>
-        </tbody>
-      </table>
+            </td>
+          </tr>
+          <tr>
+            <td colspan="2" class="p-0 ">
+            </td>
+          </tr>
+          <tr v-if="netInfo">
+            <td class="bg-light-subtle align-middle">Network-Transmitted</td>
+            <td>{{ transmittedMb }} MB</td>
+          </tr>
+          <tr v-if="netInfo">
+            <td class="bg-light-subtle align-middle">Network-Received</td>
+            <td>{{ receivedMb }} MB</td>
+          </tr>
+          <tr>
+            <td colspan="2" class="p-0 ">
+            </td>
+          </tr>
+          <tr v-for="version in versions">
+            <td class="bg-light-subtle align-middle">
+              {{ version[0] }}
+            </td>
+            <td class="align-middle">{{ version[1] }}</td>
+          </tr>
+          </tbody>
+        </table>
+      </div>
     </LoadIndicator>
   </div>
 </template>
@@ -138,6 +126,13 @@ const api = createClient();
 
 const build = inject('build');
 const version = inject('version');
+
+const versions = computed(() => [
+  [ "Client-Version", version ],
+  [ "Client-Revision", build ],
+  [ "Server-Version", serverInfo.value?.version ],
+  [ "Server-Revision", serverInfo.value?.commit ],
+]);
 
 const loaded = ref(false);
 
