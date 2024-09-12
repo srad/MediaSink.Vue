321606aaa8ab27ad354fb427ccd007b0780f16d6
Sam <ssedighi@posteo.de>
2024-09-12 03:34:35 +0200

    Create docker-entrypoint-ssr.sh

---
+++ docker-entrypoint-ssr.sh
@@ -0,0 +1,22 @@
+#!/bin/sh
+
+cat >/app/dist/client/env.js <<EOL
+window.VUE_APP_APIURL = "${APP_API_URL}";
+window.VUE_APP_BASE = "${APP_BASE}";
+window.VUE_APP_NAME = "${APP_NAME}";
+window.VUE_APP_SOCKETURL = "${APP_SOCKETURL}";
+window.VUE_APP_FILEURL = "${APP_FILEURL}";
+EOL
+
+cat >/app/.env <<EOL
+VITE_VUE_APP_APIURL=${APP_API_URL}
+VITE_VUE_APP_BASE=${APP_BASE}
+VITE_VUE_APP_NAME=${APP_NAME}
+VITE_VUE_APP_SOCKETURL=${APP_SOCKETURL}
+VITE_VUE_APP_FILEURL=${APP_FILEURL}
+VITE_VUE_APP_BUILD=${APP_BUILD}
+VITE_VUE_APP_VERSION=${APP_VERSION}
+EOL
+
+cd /app
+node --env-file=.env server.js
