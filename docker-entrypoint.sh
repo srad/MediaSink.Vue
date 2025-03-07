cfeb309a91b2842ad0b6824be0e4385f68b0ff30
srad <ssedighi@posteo.de>
2025-03-07 14:08:18 +0100

    Dockerfile

--- docker-entrypoint.sh
+++ docker-entrypoint.sh
@@ -1,11 +1,11 @@
 #!/bin/sh
 
 cat >/usr/share/nginx/html/env.js <<EOL
-window.VUE_APP_APIURL = "${APP_API_URL}";
+window.UE_APP_APIURL = "${APP_API_URL}";
 window.VUE_APP_BASE = "${APP_BASE}";
 window.VUE_APP_NAME = "${APP_NAME}";
-window.VUE_APP_SOCKETURL = "${APP_SOCKETURL}";
-window.VUE_APP_FILEURL = "${APP_FILEURL}";
+window.VUE_APP_SOCKETURL = "${APP_SOCKET_URL}";
+window.VUE_APP_FILEURL = "${APP_FILE_URL}";
 EOL
 
 nginx -g "daemon off;"
