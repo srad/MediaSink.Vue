28bc54d1a899610587bf7c2c84a8209277c31fde
srad <ssedighi@posteo.de>
2025-03-21 19:29:41 +0100

    Reactive job store provisions.

--- src/stores/job.ts
+++ src/stores/job.ts
@@ -16,6 +16,11 @@ export const useJobStore = defineStore("job", {
     getOpen(): Job[] {
       return (this.jobs || []).filter((x: Job) => x.status === DatabaseJobStatus.StatusJobOpen);
     },
+    isProcessing(): (recordingId: number) => string | null {
+      return (recordingId: number) => {
+        return this.jobs.find((job: Job) => job.recordingId === recordingId)?.task || null;
+      };
+    },
   },
   actions: {
     create(job: Job) {
@@ -25,7 +30,6 @@ export const useJobStore = defineStore("job", {
     dec() {
       this.jobsCount = Math.max(this.jobsCount - 1, 0);
     },
-    // Just load 100 jobs for the initial state.
     deleteChannel(channelId: number) {
       this.jobs = this.jobs.filter((x: Job) => x.channelId !== channelId);
     },
@@ -33,7 +37,10 @@ export const useJobStore = defineStore("job", {
       this.jobs = this.jobs.filter((x: Job) => x.recordingId !== recordingId);
     },
     destroy(jobId: number) {
-      this.jobs = this.jobs.filter((x: Job) => x.jobId !== jobId);
+      const index = this.jobs.findIndex((x: Job) => x.jobId === jobId);
+      if (index !== -1) {
+        this.jobs.splice(index, 1);
+      }
     },
     done(message: JobMessage<TaskComplete>) {
       this.destroy(message.job.jobId);
@@ -42,41 +49,37 @@ export const useJobStore = defineStore("job", {
     inactive(job: Job) {
       const i = this.jobs.findIndex((j: Job) => j.jobId === job.jobId);
       if (i !== -1) {
-        this.jobs[i]!.active = false;
+        this.jobs[i] = { ...this.jobs[i], active: false };
       }
     },
     progress(message: JobMessage<TaskProgress>) {
       const i = this.jobs.findIndex((j: Job) => j.jobId === message.job.jobId);
       const progress = String((((message.data.step / message.data.steps) * message.data.current) / message.data.total) * 100);
+
       if (i !== -1) {
-        this.jobs[i]!.progress = progress;
-        this.jobs[i]!.info = message.data.message;
+        this.jobs[i] = { ...this.jobs[i], progress, info: message.data.message };
       } else {
-        this.jobs.unshift(message.job);
-        this.jobs[0]!.progress = progress;
-        this.jobs[0]!.info = message.data.message;
+        this.jobs.unshift({ ...message.job, progress, info: message.data.message });
       }
     },
     refresh(data: { jobs: Job[]; totalCount: number }) {
-      this.jobs = data.jobs;
+      this.jobs = [...data.jobs];
       this.jobsCount = data.totalCount;
     },
     start(message: JobMessage<TaskInfo>) {
       let i = this.jobs.findIndex((j: Job) => j.jobId === message.job.jobId);
+
       if (i === -1) {
         this.jobs.unshift(message.job);
         i = 0;
       }
-      this.jobs[i]!.active = true;
-      this.jobs[i]!.pid = message.data.pid;
-      this.jobs[i]!.command = message.data.command;
+
+      this.jobs[i] = {
+        ...this.jobs[i],
+        active: true,
+        pid: message.data.pid,
+        command: message.data.command,
+      };
     },
-    isProcessing(recordingId: number): string | null {
-      const job = this.jobs.find((job: Job) => job.recordingId === recordingId);
-      if (job) {
-        return job.task;
-      }
-      return null;
-    }
   },
 });
