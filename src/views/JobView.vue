99057d420796aff1c757a09da67784cfe11e23a0
srad <ssedighi@posteo.de>
2025-03-26 17:30:11 +0100

    Migrating jobs and process table to DataTable. DataTable show test 'empty' if no results

--- src/views/JobView.vue
+++ src/views/JobView.vue
@@ -16,7 +16,16 @@
     </ModalConfirmDialog>
     <div class="row mb-2">
       <div class="col">
-        <div class="d-flex justify-content-end">
+        <div class="d-flex justify-content-between">
+          <div class="d-flex">
+            <select class="form-control form-select" v-model="tab">
+              <option class="fw-bold" disabled readonly value="">Job types</option>
+              <option v-for="tab in tabs" :value="tab.key" :key="tab.key">
+                {{ tab.value }}
+              </option>
+            </select>
+          </div>
+
           <div class="d-flex justify-content-center">
             <div class="col-auto">
               <button type="button" class="btn me-2" :class="{ 'btn-success': !processingJobs, 'btn-danger': processingJobs }" @click="showConfirmToggleWorkerDialog = true">
@@ -24,156 +33,94 @@
                 <span v-else><i class="bi bi-play-fill" /> <span class="ms-1 d-none d-sm-inline-flex">Resume</span></span>
               </button>
             </div>
-            <!-- filter row -->
-            <div class="row align-items-center">
-              <div class="col-auto">
-                <select id="limit" class="form-select" v-model="take">
-                  <option value="" style="font-weight: bold" disabled>{{ t("filter.limit") }}</option>
-                  <option v-for="limit in limits" :key="limit" :value="limit">{{ limit }}</option>
-                </select>
-              </div>
-
-              <div class="col-auto">
-                <button type="button" class="btn btn-primary" @click="resetFilters">
-                  {{ t("filter.reset") }}
-                </button>
-              </div>
-            </div>
           </div>
         </div>
       </div>
     </div>
 
-    <ul class="nav nav-tabs my-2" id="pills-tab" role="tablist">
-      <li class="nav-item" role="presentation">
-        <RouterLink class="text-decoration-none" to="/jobs/general">
-          <button data-tab="general" class="nav-link" :class="{ active: route.params.tab === 'general' }" id="pills-open-tab" data-bs-toggle="pill" data-bs-target="#pills-open" type="button" role="tab" aria-controls="pills-open" aria-selected="true">{{ t("general.open") }} <i class="bi bi-arrow-clockwise" /></button>
-        </RouterLink>
-      </li>
-      <li class="nav-item" role="presentation">
-        <RouterLink to="/jobs/completed" class="text-decoration-none">
-          <button data-tab="completed" class="nav-link" :class="{ active: route.params.tab === 'completed' }" id="pills-completed-tab" data-bs-toggle="pill" data-bs-target="#pills-completed" type="button" role="tab" aria-controls="pills-completed" aria-selected="false">{{ t("general.completed") }} <i class="bi bi-check2-all" /></button>
-        </RouterLink>
-      </li>
-      <li class="nav-item" role="presentation">
-        <RouterLink to="/jobs/other" class="text-decoration-none">
-          <button data-tab="other" class="nav-link" :class="{ active: route.params.tab === 'other' }" id="pills-other-tab" data-bs-toggle="pill" data-bs-target="#pills-other" type="button" role="tab" aria-controls="pills-other" aria-selected="false">
-            {{ t("general.other") }}
-            <i class="bi bi-question" />
-          </button>
-        </RouterLink>
-      </li>
-      <li class="nav-item" role="presentation">
-        <RouterLink to="/jobs/processes" class="text-decoration-none">
-          <button data-tab="streams" class="nav-link" :class="{ active: route.params.tab === 'processes' }" id="pills-processes-tab" data-bs-toggle="pill" data-bs-target="#pills-processes" type="button" role="tab" aria-controls="pills-processes" aria-selected="false">{{ t("general.streams") }} <i class="bi bi-camera-video" /></button>
-        </RouterLink>
-      </li>
-    </ul>
-    <div class="tab-content" id="pills-tabContent">
-      <div data-tab="general" class="tab-pane fade" :class="{ 'show active': route.params.tab === 'general' }" id="pills-open" role="tabpanel" aria-labelledby="pills-open-tab">
-        <JobTable :jobs="itemsOpen" @destroy="destroy" :total-count="itemsCount" :show-progress="true" />
-      </div>
-
-      <div data-tab="completed" class="tab-pane fade" :class="{ 'show active': route.params.tab === 'completed' }" id="pills-completed" role="tabpanel" aria-labelledby="pills-completed-tab">
-        <JobTable :jobs="itemsCompleted" @destroy="destroy" :total-count="itemsCompletedCount" :show-progress="false" />
-      </div>
-
-      <div data-tab="other" class="tab-pane fade" :class="{ 'show active': route.params.tab === 'other' }" id="pills-other" role="tabpanel" aria-labelledby="pills-other-tab">
-        <JobTable :jobs="itemsOther" @destroy="destroy" :total-count="itemsCompletedCount" :show-progress="false" :show-info="true" />
-      </div>
-
-      <div data-tab="streams" class="tab-pane fade" :class="{ 'show active': route.params.tab === 'processes' }" id="pills-processes" role="tabpanel" aria-labelledby="pills-processes-tab">
-        <div class="table-responsive">
-          <table class="table table-bordered">
-            <thead>
-              <tr>
-                <th class="bg-light" style="width: 10%">Channel-Id</th>
-                <th class="bg-light" style="width: 10%">Pid</th>
-                <th class="bg-light" style="width: 10%">Path</th>
-                <th class="bg-light">Args</th>
-                <th class="bg-light">Output</th>
-              </tr>
-            </thead>
-            <tbody>
-              <tr v-if="processes.length === 0">
-                <td colspan="5">None</td>
-              </tr>
-              <tr v-else v-for="p in processes" :key="p.id">
-                <td>{{ p.id }}</td>
-                <td>{{ p.pid }}</td>
-                <td>{{ p.path }}</td>
-                <td>
-                  <textarea disabled class="form-control" rows="5" v-model="p.args"></textarea>
-                </td>
-                <td>
-                  <textarea disabled class="form-control" rows="5" v-model="p.output"></textarea>
-                </td>
-              </tr>
-            </tbody>
-          </table>
-        </div>
-      </div>
-    </div>
+    <DataTable v-if="tab === 'processes'" :columns="processesCols" :data="processes">
+      <template #cell-args="{ value }">
+        <textarea disabled readonly class="form-control" v-model="value as string" />
+      </template>
+    </DataTable>
+    <JobTable v-else :jobs="jobs" @destroy="destroy" :total-count="jobs.length" :show-progress="true" />
   </div>
 </template>
 
 <script setup lang="ts">
-import type { DatabaseJob, ResponsesJobsResponse, ServicesProcessInfo as ProcessInfo } from "@/services/api/v1/StreamSinkClient";
-import { DatabaseJobOrder, DatabaseJobStatus } from "@/services/api/v1/StreamSinkClient";
-import { fromNow } from "@/utils/datetime";
+import { type DatabaseJob, DatabaseJobOrder, DatabaseJobStatus, type ServicesProcessInfo } from "@/services/api/v1/StreamSinkClient";
 import { useJobStore } from "@/stores/job";
 import ModalConfirmDialog from "@/components/modals/ModalConfirmDialog.vue";
-import { computed, onMounted, ref } from "vue";
-import { useI18n } from "vue-i18n";
+import { onMounted, ref, watch } from "vue";
 import JobTable, { type JobTableItem } from "@/components/JobTable.vue";
-import { useRoute } from "vue-router";
+import { useRoute, useRouter } from "vue-router";
 import { createClient } from "@/services/api/v1/ClientFactory";
-
-const { t } = useI18n();
+import { fromNow } from "@/utils/datetime.ts";
+import DataTable, { type Column } from "@/components/DataTable.vue";
 
 const jobStore = useJobStore();
 const route = useRoute();
-const processes = ref<ProcessInfo[]>([]);
+const router = useRouter();
 const processingJobs = ref(true);
 const showConfirmToggleWorkerDialog = ref(false);
+const tab = ref<JobType>(route.params.tab as JobType);
 
-const skip = ref(0);
-const take = ref(50);
-const limits = [25, 50, 100, 200];
+const processesCols: Column[] = [
+  { label: "PID", key: "pid", width: "10%" },
+  { label: "Path", key: "path", width: "20%" },
+  { label: "Args", key: "args", width: "width: 70%" },
+];
 
-const resetFilters = () => {
-  skip.value = 0;
-  take.value = 50;
+const processes = ref<ServicesProcessInfo[]>([]);
+
+const tabs: { key: JobType; value: string }[] = [
+  { key: "open", value: "Open" },
+  { key: "completed", value: "Completed" },
+  { key: "canceled", value: "Canceled" },
+  { key: "error", value: "Errors" },
+  { key: "processes", value: "Processes" },
+];
+
+watch(tab, (tab: JobType) => {
+  router.push(`/jobs/${tab}`);
+});
+
+type JobType = "open" | "completed" | "canceled" | "error" | "processes";
+
+const tabToJobType: { [key: string]: DatabaseJobStatus } = {
+  open: DatabaseJobStatus.StatusJobOpen,
+  completed: DatabaseJobStatus.StatusJobCompleted,
+  canceled: DatabaseJobStatus.StatusJobCanceled,
+  processes: DatabaseJobStatus.StatusJobError,
 };
 
-const jobsCompleted = ref<ResponsesJobsResponse | null>(null);
-const jobsOther = ref<ResponsesJobsResponse | null>(null);
+const jobs = ref<JobTableItem[]>([]);
 
-const addFromNowToJob = (job: DatabaseJob): JobTableItem => {
-  const newJob: JobTableItem = {
-    createdAtFromNow: fromNow(Date.parse(job.createdAt)),
-    startedFromNow: job.startedAt ? fromNow(Date.parse(job.startedAt)) : "-",
-    completedAtFromNow: job.completedAt ? fromNow(Date.parse(job.completedAt)) : "-",
-    ...job,
-  };
+onMounted(async () => {
+  await load();
+});
 
-  if (job.completedAt && job.startedAt) {
-    const end = Date.parse(job.completedAt);
-    const start = Date.parse(job.startedAt);
-    const diffMS = end - start;
-    newJob.jobDuration = `${(diffMS / 1000 / 60).toFixed(1)}min`;
+const load = async () => {
+  const client = createClient();
+
+  if (tab.value === "open" || tab.value === "completed" || tab.value === "canceled" || tab.value === "error") {
+    const result = await client.jobs.listCreate({
+      states: [tabToJobType[tab.value]],
+      sortOrder: DatabaseJobOrder.JobOrderDESC,
+      take: 200,
+    });
+    if (result.jobs) {
+      jobs.value = result.jobs.map((job: DatabaseJob) => ({
+        ...job,
+        createdAtFromNow: fromNow(Date.parse(job.createdAt)),
+        startedFromNow: job.startedAt ? fromNow(Date.parse(job.startedAt)) : "-",
+        completedAtFromNow: job.completedAt ? fromNow(Date.parse(job.completedAt)) : "-",
+      }));
+    }
+  } else if (tab.value === "processes") {
+    processes.value = await client.processes.processesList();
   }
-  return newJob;
 };
-
-const itemsOpen = computed(() => jobStore.open.map(addFromNowToJob));
-const itemsCount = computed(() => jobStore.jobsCount);
-
-const itemsCompleted = computed(() => (jobsCompleted.value?.jobs || []).map(addFromNowToJob));
-const itemsCompletedCount = computed(() => jobsCompleted.value?.totalCount || 0);
-
-const itemsOther = computed(() => (jobsOther.value?.jobs || []).map(addFromNowToJob));
-//const itemsOtherCount = computed(() => jobsOther.value?.totalCount || 0);
 
 const destroy = (id: number) => {
   if (window.confirm("Delete?")) {
@@ -196,54 +143,4 @@ const toggleWorker = () => {
     .catch((res) => alert((<{ error: string }>res).error))
     .finally(() => (showConfirmToggleWorkerDialog.value = false));
 };
-
-onMounted(async () => {
-  const client = createClient();
-
-  const data = await Promise.all([
-    client.jobs.listCreate({
-      skip: skip.value,
-      take: take.value,
-      states: [DatabaseJobStatus.StatusJobOpen],
-      sortOrder: DatabaseJobOrder.JobOrderASC,
-    }),
-    client.jobs.listCreate({
-      skip: skip.value,
-      take: take.value,
-      states: [DatabaseJobStatus.StatusJobCompleted],
-      sortOrder: DatabaseJobOrder.JobOrderDESC,
-    }),
-    client.jobs.listCreate({
-      skip: skip.value,
-      take: take.value,
-      states: [DatabaseJobStatus.StatusJobCanceled, DatabaseJobStatus.StatusJobCanceled, DatabaseJobStatus.StatusJobError],
-      sortOrder: DatabaseJobOrder.JobOrderDESC,
-    }),
-    client.processes.processesList(),
-    client.jobs.workerList(),
-  ]);
-
-  if (data) {
-    const open = data[0];
-    const completed = data[1];
-    const others = data[2];
-    const process = data[3];
-    const jobWorker = data[4];
-
-    if (open.jobs) {
-      jobStore.refresh({ jobs: open.jobs, totalCount: open.totalCount });
-    }
-
-    if (completed.jobs) {
-      jobsCompleted.value = completed;
-    }
-
-    if (others.jobs) {
-      jobsOther.value = others;
-    }
-
-    processes.value = process || [];
-    processingJobs.value = jobWorker.isProcessing;
-  }
-});
 </script>
